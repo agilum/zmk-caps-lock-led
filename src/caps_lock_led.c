e8a7e2cd7e52d3bbaa6fbf35b6d3a30f1710dea5
agilum <agilum@gmail.com>
2025-11-23 20:20:58 +0200

    Update caps_lock_led.c

--- src/caps_lock_led.c
+++ src/caps_lock_led.c
@@ -5,22 +5,33 @@
 #include <zephyr/logging/log.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/hid_indicators_changed.h>
-#include <zmk/hid_indicators_types.h>
+#include <zmk/hid.h>  // For BIT(1) if needed, but use ZMK_HID_INDICATOR_CAPS_LOCK if available
 
-LOG_MODULE_REGISTER(caps_lock_led, CONFIG_ZMK_LOG_LEVEL);
+LOG_MODULE_REGISTER(caps_lock_led, LOG_LEVEL_DBG);  // DBG for verbose output
 
 static const struct device *led_dev = DEVICE_DT_GET(DT_NODELABEL(caps_leds));
 
-// Forward declaration to resolve visibility warning
-struct zmk_event_header;
+static int init_caps_lock_led(void) {
+    if (!device_is_ready(led_dev)) {
+        LOG_ERR("Caps lock LED device not ready at boot");
+        return -ENODEV;
+    }
+    LOG_INF("Caps lock LED module initialized successfully");
+    return 0;
+}
+
+struct zmk_event_header;  // Forward declaration
 
 static int caps_lock_callback(const struct zmk_event_header *eh) {
     struct zmk_hid_indicators_changed *ev = (struct zmk_hid_indicators_changed *)eh;
+    LOG_DBG("HID indicators changed event received: indicators = 0x%02X", ev->indicators);
 
     if (device_is_ready(led_dev)) {
-        if (ev->indicators & BIT(1)) {  // BIT(1) = Caps Lock (0x02)
+        if (ev->indicators & BIT(1)) {  // Caps Lock bit (0x02)
+            LOG_DBG("Turning caps lock LED ON");
             led_on(led_dev, 0);
         } else {
+            LOG_DBG("Turning caps lock LED OFF");
             led_off(led_dev, 0);
         }
     } else {
@@ -32,3 +43,5 @@ static int caps_lock_callback(const struct zmk_event_header *eh) {
 
 ZMK_LISTENER(caps_lock_led_listener, caps_lock_callback);
 ZMK_SUBSCRIPTION(caps_lock_led_listener, zmk_hid_indicators_changed);
+
+SYS_INIT(init_caps_lock_led, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);  // Boot init
