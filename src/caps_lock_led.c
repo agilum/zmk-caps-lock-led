9603236969579db900564a9b1c77f717ad7e8b26
agilum <agilum@gmail.com>
2025-11-23 10:19:20 +0200

    Update caps_lock_led.c

--- src/caps_lock_led.c
+++ src/caps_lock_led.c
@@ -3,19 +3,22 @@
 #include <zephyr/drivers/led.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
-#include <zmk/event_manager.h>  // For struct zmk_event_header, ZMK_LISTENER, ZMK_SUBSCRIPTION
-#include <zmk/events/hid_indicators_changed.h>  // For the event struct
-#include <zmk/hid.h>  // For ZMK_HID_INDICATOR_CAPS_LOCK
+#include <zmk/event_manager.h>
+#include <zmk/events/hid_indicators_changed.h>
+#include <zmk/hid_indicators_types.h>
 
 LOG_MODULE_REGISTER(caps_lock_led, CONFIG_ZMK_LOG_LEVEL);
 
 static const struct device *led_dev = DEVICE_DT_GET(DT_NODELABEL(caps_leds));
 
+// Forward declaration to resolve visibility warning
+struct zmk_event_header;
+
 static int caps_lock_callback(const struct zmk_event_header *eh) {
     struct zmk_hid_indicators_changed *ev = (struct zmk_hid_indicators_changed *)eh;
 
     if (device_is_ready(led_dev)) {
-        if (ev->indicators & ZMK_HID_INDICATOR_CAPS_LOCK) {
+        if (ev->indicators & BIT(1)) {  // BIT(1) = Caps Lock (0x02)
             led_on(led_dev, 0);
         } else {
             led_off(led_dev, 0);
